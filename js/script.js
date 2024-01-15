093c86866228dd57291ee159edc3123296513c9b
Varun-KC <kushchoudhary8430@gmail.com>
2024-01-15 19:41:51 +0530

    added songs

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ async function getSongs(folder) {
  
 
 
-    // Show all the songs in the playlist
+    
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     songUL.innerHTML = ""
     for (const song of songs) {
@@ -46,7 +46,7 @@ async function getSongs(folder) {
                             </div> </li>`;
     }
 
-    // Attach an event listener to each song
+    
     Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
         e.addEventListener("click", element => {
             playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim() + ".mp3")
@@ -99,7 +99,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs/") && (await getSongsList(e.href)).length != 0) {
             
             folder = e.href.split("/").slice(-1)[0]
-            // Get the metadata of the folder
+           
             let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
@@ -118,7 +118,7 @@ async function displayAlbums() {
         }
     }
 
-    // Load the playlist whenever card is clicked
+    
     Array.from(document.getElementsByClassName("card")).forEach(e => { 
         e.addEventListener("click", async item => {
             songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`) 
@@ -127,11 +127,8 @@ async function displayAlbums() {
 }
 
 async function main() {
-    // Display all the albums on the page
     await displayAlbums()
 
-
-    // Attach an event listener to play, next and previous
     play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play()
@@ -142,30 +139,26 @@ async function main() {
             play.src = "img/play.svg"
         }
     })
-    // Listen for timeupdate event
+    
     currentSong.addEventListener("timeupdate", () => {
         document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
     })
 
-    // Add an event listener to seekbar
     document.querySelector(".seekbar").addEventListener("click", e => {
         let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
         document.querySelector(".circle").style.left = percent + "%";
         currentSong.currentTime = ((currentSong.duration) * percent) / 100
     })
 
-    // Add an event listener for hamburger
     document.querySelector(".hamburger").addEventListener("click", () => {
         document.querySelector(".left").style.left = "0"
     })
 
-    // Add an event listener for close button
     document.querySelector(".close").addEventListener("click", () => {
         document.querySelector(".left").style.left = "-120%"
     })
 
-    // Add an event listener to previous
     previous.addEventListener("click", () => {
         currentSong.pause()
         let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
@@ -174,7 +167,7 @@ async function main() {
         }
     })
 
-    // Add an event listener to next
+    
     next.addEventListener("click", () => {
         currentSong.pause()
 
@@ -184,7 +177,7 @@ async function main() {
         }
     })
 
-    // Add an event to volume
+
     document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
         currentSong.volume = parseInt(e.target.value) / 100
         if (currentSong.volume >0){
@@ -192,7 +185,7 @@ async function main() {
         }
     })
 
-    // Add event listener to mute the track
+ 
     document.querySelector(".volume>img").addEventListener("click", e=>{ 
         if(e.target.src.includes("volume.svg")){
             e.target.src = e.target.src.replace("volume.svg", "mute.svg")
